9c0cdf7a78e9a58709b809aa77dff4fd53d24add
hyewonk815 <140413107+hyewonk815@users.noreply.github.com>
2023-09-24 20:33:01 +0900

    commit -6.17

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,8 @@
 import Video from '../models/Video';
 
-export const home = (req, res) => {
-  console.log('Start');
-  Video.find({}, (error, videos) => {
-    console.log('Finished');
-    return res.render('home', { pageTitle: 'Home', videos });
-  });
-  console.log('I finish first');
+export const home = async (req, res) => {
+  const videos = await Video.find({});
+  return res.render('home', { pageTitle: 'Home', videos });
 };
 
 export const watch = (req, res) => {
@@ -28,7 +24,19 @@ export const getUpload = (req, res) => {
   return res.render('upload', { pageTitle: 'Upload Video' });
 };
 
-export const postUpload = (req, res) => {
-  const { title } = req.body;
-  return res.redirect('/');
+export const postUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body;
+  try {
+    await Video.create({
+      title,
+      description,
+      hashtags: hashtags.split(',').map((word) => `#${word}`),
+    });
+    return res.redirect('/');
+  } catch (error) {
+    return res.render('upload', {
+      pageTitle: 'Upload Video',
+      errorMessage: error._message,
+    });
+  }
 };
