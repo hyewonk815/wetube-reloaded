cc66908e23ef39f684ad8925ec566d72185f5146
hyewonk815 <140413107+hyewonk815@users.noreply.github.com>
2023-09-29 21:48:57 +0900

    commit -15.1

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -24,6 +24,7 @@ export const getEdit = async (req, res) => {
     return res.status(404).render('404', { pageTitle: 'Video not found.' });
   }
   if (String(video.owner) !== String(_id)) {
+    req.flash('error', 'Not authorized');
     return res.status(403).redirect('/');
   }
   return res.render('edit', { pageTitle: `Edit: ${video.title}`, video });
@@ -39,6 +40,7 @@ export const postEdit = async (req, res) => {
     return res.status(404).render('404', { pageTitle: 'Video not found.' });
   }
   if (String(video.owner) !== String(_id)) {
+    req.flash('error', 'You are not the the owner of the video.');
     return res.status(403).redirect('/');
   }
   await Video.findByIdAndUpdate(id, {
@@ -46,6 +48,7 @@ export const postEdit = async (req, res) => {
     description,
     hashtags: Video.formatHashtags(hashtags),
   });
+  req.flash('success', 'Changes saved.');
   return res.redirect(`/videos/${id}`);
 };
 export const getUpload = (req, res) => {
@@ -56,13 +59,14 @@ export const postUpload = async (req, res) => {
   const {
     user: { _id },
   } = req.session;
-  const { path: fileUrl } = req.file;
+  const { video, thumb } = req.files;
   const { title, description, hashtags } = req.body;
   try {
     const newVideo = await Video.create({
       title,
       description,
-      fileUrl,
+      fileUrl: video[0].path,
+      thumbUrl: thumb[0].path,
       owner: _id,
       hashtags: Video.formatHashtags(hashtags),
     });
