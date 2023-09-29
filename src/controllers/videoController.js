72a480e9b36126c2e4d67b747d56eb3eb280422a
hyewonk815 <140413107+hyewonk815@users.noreply.github.com>
2023-09-29 22:43:16 +0900

    commit -16.8

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,5 @@
 import Video from '../models/Video';
+import Comment from '../models/Comment';
 import User from '../models/User';
 
 export const home = async (req, res) => {
@@ -7,13 +8,13 @@ export const home = async (req, res) => {
 };
 export const watch = async (req, res) => {
   const { id } = req.params;
-  const video = await Video.findById(id).populate('owner');
+  const video = await Video.findById(id).populate('owner').populate('comments');
+  console.log(video);
   if (!video) {
     return res.render('404', { pageTitle: 'Video not found.' });
   }
   return res.render('watch', { pageTitle: video.title, video });
 };
-
 export const getEdit = async (req, res) => {
   const { id } = req.params;
   const {
@@ -30,10 +31,10 @@ export const getEdit = async (req, res) => {
   return res.render('edit', { pageTitle: `Edit: ${video.title}`, video });
 };
 export const postEdit = async (req, res) => {
-  const { id } = req.params;
   const {
     user: { _id },
   } = req.session;
+  const { id } = req.params;
   const { title, description, hashtags } = req.body;
   const video = await Video.exists({ _id: id });
   if (!video) {
@@ -54,7 +55,6 @@ export const postEdit = async (req, res) => {
 export const getUpload = (req, res) => {
   return res.render('upload', { pageTitle: 'Upload Video' });
 };
-
 export const postUpload = async (req, res) => {
   const {
     user: { _id },
@@ -117,5 +117,25 @@ export const registerView = async (req, res) => {
   }
   video.meta.views = video.meta.views + 1;
   await video.save();
-  return res.sendStatus(404);
+  return res.sendStatus(200);
+};
+
+export const createComment = async (req, res) => {
+  const {
+    session: { user },
+    body: { text },
+    params: { id },
+  } = req;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.sendStatus(404);
+  }
+  const comment = await Comment.create({
+    text,
+    owner: user._id,
+    video: id,
+  });
+  video.comments.push(comment._id);
+  video.save();
+  return res.status(201).json({ newCommentId: comment._id });
 };
