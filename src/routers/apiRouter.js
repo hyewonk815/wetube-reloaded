afdb4081b80940a6a2836b4fb63ea73827b6fa60
hyewonk815 <140413107+hyewonk815@users.noreply.github.com>
2023-10-02 02:06:00 +0900

    commit

--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -1,10 +1,9 @@
 import express from 'express';
 import { registerView, createComment, deleteComment } from '../controllers/videoController';
-
 const apiRouter = express.Router();
 
 apiRouter.post('/videos/:id([0-9a-f]{24})/view', registerView);
 apiRouter.post('/videos/:id([0-9a-f]{24})/comment', createComment);
-apiRouter.delete('/videos/:videoId([0-9a-f]{24})/comments/:id([0-9a-f]{24})/delete', deleteComment);
+apiRouter.delete('/videos/:id([0-9a-f]{24})/comment/delete', deleteComment);
 
 export default apiRouter;
